1e03a05d93dddb71e74c231cd9daec0d6e24bfec
Christian Sasso <christian.sasso@gmail.com>
2014-02-19 05:52:06 -0800

    Added enemies that follow player and colisions between player and enemies and enemies and bullets.

---
+++ Assets/CollisionDetection.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetection : MonoBehaviour {
+
+	public string collidedTo;
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == collidedTo)
+		{
+			Destroy(gameObject);
+			Destroy (collision.gameObject);
+		}
+	}
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public float interval = 2.0f;
+	public GameObject creature;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("Spawn");
+	}
+
+	IEnumerator Spawn () {
+		for (;;) {
+			Vector2 newPosition2D = Random.insideUnitCircle;
+			Vector3 newPosition3D = transform.position;
+			newPosition3D.x += newPosition2D.x;
+			newPosition3D.y = 3;
+			newPosition3D.z += newPosition2D.y;
+			GameObject spawned = Instantiate(creature, newPosition3D, transform.rotation) as GameObject;
+			yield return new WaitForSeconds(interval);
+		}
+	}
+}
