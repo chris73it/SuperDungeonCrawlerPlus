44bade7345963bdbbe6c734df6db683ba1392359
Christian Sasso <christian.sasso@gmail.com>
2014-02-19 20:11:53 -0800

    Player can pick up key and go to he next level.

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	public string collidedTo;
+
+	private bool upping = false;
+	
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log ("OnCollisionEnter: " + collision.gameObject.tag + " - " + collidedTo);
+
+		if (PickUp.gotKey == false) { return;  }
+
+		if (collision.gameObject.tag == collidedTo && upping == false)
+		{
+			upping = true;
+			StartCoroutine("GoToNextLevel", collision.gameObject);
+		}
+	}
+
+	IEnumerator GoToNextLevel (GameObject go) {
+		Vector3 newPosition = transform.position;
+
+		yield return new WaitForSeconds(1.0f);
+
+		for (float y = 5.0f; y < 500.0f; y+=5.0f) {
+
+			newPosition.y += y/500.0f;
+			transform.position = newPosition;
+			go.transform.position = newPosition;
+
+			yield return new WaitForSeconds(0.016f);
+		}
+		Destroy (go.gameObject);//FIXME: go to next level
+		Destroy (gameObject);//FIXME: go to next level
+	}
+}
