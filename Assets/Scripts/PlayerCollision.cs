5ab16128d530c86c361f37bb1f6b50b13b1e5a59
Christian Sasso <christian.sasso@gmail.com>
2014-02-19 18:41:36 -0800

    Merged the 2 coroutines used for the death scene into one.

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -9,30 +9,26 @@ public class PlayerCollision : MonoBehaviour {
 	{
 		if (collision.gameObject.tag == collidedTo)
 		{
-			StartCoroutine("FlyToHeaven");
-			StartCoroutine("Fade");
+			StartCoroutine("FlyToHeavenWhileFading");
 		}
 	}
 	
-	IEnumerator FlyToHeaven() {
+	IEnumerator FlyToHeavenWhileFading () {
 		Vector3 newPosition = transform.position;
-		
-		for (float y = 1.0f; y < 200f; y+=1.0f) {
-			newPosition.y += y/100.0f;
+		Color c = renderer.material.color;
+		float a, y;
+
+		for (a = 80.0f, y = 5.0f; y < 500.0f; y+=5.0f) {
+
+			c.a = a/100.0f;
+			renderer.material.color = c;
+			if (a > 0.01f) { a--; } else { a = 0.0f; }
+
+			newPosition.y += y/500.0f;
 			transform.position = newPosition;
-			
+
 			yield return new WaitForSeconds(0.016f);
 		}
 		Destroy (gameObject);
-	}
-
-	IEnumerator Fade() {
-		Color c = renderer.material.color;
-
-		for (float a = 80.0f; a >= 0.0f; a--) {
-			c.a = a/100.0f;
-			renderer.material.color = c;
-			yield return new WaitForSeconds(0.016f);
-		}
 	}
 }
