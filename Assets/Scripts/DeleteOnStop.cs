f3c349d73dec98396dbc180b6c4d9717d70bc4f6
Christian Sasso <christian.sasso@gmail.com>
2014-02-19 18:20:32 -0800

    Reorganized Assets, modified death sequence to avoid enemies asserting trying to chase an inexisting player.

---
+++ Assets/Scripts/DeleteOnStop.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteOnStop : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigidbody.velocity == Vector3.zero) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+	public string collidedTo;
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == collidedTo)
+		{
+			StartCoroutine("FlyToHeaven");
+			StartCoroutine("Fade");
+		}
+	}
+	
+	IEnumerator FlyToHeaven() {
+		Vector3 newPosition = transform.position;
+		
+		for (float y = 1.0f; y < 200f; y+=1.0f) {
+			newPosition.y += y/100.0f;
+			transform.position = newPosition;
+			
+			yield return new WaitForSeconds(0.016f);
+		}
+		Destroy (gameObject);
+	}
+
+	IEnumerator Fade() {
+		Color c = renderer.material.color;
+
+		for (float a = 80.0f; a >= 0.0f; a--) {
+			c.a = a/100.0f;
+			renderer.material.color = c;
+			yield return new WaitForSeconds(0.016f);
+		}
+	}
+}
