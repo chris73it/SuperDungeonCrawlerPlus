bd4c77aa2470c08e5532d9be76c5181df60c6597
Christian Sasso <christian.sasso@gmail.com>
2014-02-19 20:17:32 -0800

    Player can pick up key and go to he next level (second commit).

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class PickUp : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	static public bool gotKey = false;
+
+	public string collidedTo;
+
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log ("OnCollisionEnter: " + collision.gameObject.tag + " - " + collidedTo);
+		if (collision.gameObject.tag == collidedTo)
+		{
+			gotKey = true;
+			Destroy (gameObject);
+//			Vector3 newPosition = transform.position;
+//			newPosition.y = -10.0f; //FIXME: the inventory is under the floor
+//									// check the y cohordinate to know whether it
+//									// has been picked up.
+//			transform.position = newPosition;
+		}
 	}
 }
