3da7a51c792bbf7783c022fd2972f3b011934de6
Christian Sasso <christian.sasso@gmail.com>
2014-02-22 02:51:05 -0800

    Added code to center player in he middle of an exit/elevator..

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -7,13 +7,12 @@ public class Follow : MonoBehaviour {
 
 	private GameObject target;
 	private float interval = 0.1f;
-
-	// Use this for initialization
+	
 	void Start () {
 		target = GameObject.Find("Player");
 		StartCoroutine("FollowTarget");
 	}
-
+	
 	IEnumerator FollowTarget () {
 		for (;;) {
 			Vector3 fromToDir = target.transform.position - transform.position;
--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -5,24 +5,65 @@ public class NextLevel : MonoBehaviour {
 	
 	public string collidedTo;
 
+	private float speed = 4.0f;
+	private GameObject target;
+	private float interval = 0.01f;
 	private bool upping = false;
-	
+	private Collision collision;
+
+	void OnCollisionExit (Collision collision) {
+		Debug.Log ("OnCollisionExit");
+
+		upping = false;
+		Debug.Log("No longer in contact with " + collision.transform.name);
+	}
+
 	void OnCollisionEnter (Collision collision) {
-		Debug.Log ("OnCollisionEnter: " + collision.gameObject.tag + " - " + collidedTo);
+		Debug.Log ("OnCollisionEnter: " + collision.gameObject.tag);
 
 		if (PickUp.gotKey == false) { return;  }
 
 		if (collision.gameObject.tag == collidedTo && upping == false)
 		{
 			upping = true;
-			StartCoroutine("GoToNextLevel", collision.gameObject);
+			target = GameObject.Find("Player");
+			StartCoroutine("CenterTarget", collision);
 		}
 	}
 
-	IEnumerator GoToNextLevel (GameObject otherObject) {
-		Vector3 newPosition = transform.position;
+	IEnumerator CenterTarget (Collision collision) {
+		for (;;) {
+			if (upping == false) {
+				yield break;
+			}
+			Vector3 fromToDir = transform.position - target.transform.position;
+			Debug.Log ("CenterTarget: " + fromToDir.magnitude);
+			if (fromToDir.magnitude < 0.54f) {
+				if (upping == true) {
+					Vector3 newPosition = transform.position;
+					target.transform.position = newPosition;
+					for (float y = 5.0f; y < 500.0f; y+=5.0f) {
+						newPosition.y += y/500.0f;
+						transform.position = newPosition;
+						collision.gameObject.transform.position = newPosition;
+						yield return new WaitForSeconds(0.016f);
+					}
+					Destroy (gameObject);// Remove the exit/elevator
+					Application.LoadLevel(++Globals.currentLevel);
+				}
+				yield break;
+			} //else {
+				fromToDir.Normalize();
+				rigidbody.velocity = fromToDir * speed;
+				yield return new WaitForSeconds(interval);
+			//}
+		}
+	}
 
-		yield return new WaitForSeconds(1.5f);
+	/*	
+	IEnumerator GoToNextLevel (GameObject otherObject) {
+		Debug.Log ("GoToNextLevel");
+		Vector3 newPosition = transform.position;
 
 		for (float y = 5.0f; y < 500.0f; y+=5.0f) {
 
@@ -32,8 +73,8 @@ public class NextLevel : MonoBehaviour {
 
 			yield return new WaitForSeconds(0.016f);
 		}
-
 		Destroy (gameObject);// Remove the exit/elevator
 		Application.LoadLevel(++Globals.currentLevel);
 	}
+	*/
 }
