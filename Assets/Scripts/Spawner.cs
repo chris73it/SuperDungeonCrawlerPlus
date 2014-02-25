35cd9b0095adc438bba008284a7c9cfc04a5e470
Christian Sasso <christian.sasso@gmail.com>
2014-02-24 19:03:26 -0800

    Nests spawn enemies only when the hero is at a certain configurable distance.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,20 +4,26 @@ using System.Collections;
 public class Spawner : MonoBehaviour {
 
 	public float interval;
+	public GameObject target;
 	public GameObject creature;
+	public float minDistance;
 
 	void Start () {
+		target = GameObject.Find("Player");
 		StartCoroutine("Spawn");
 	}
 
 	IEnumerator Spawn () {
 		for (;;) {
-			Vector2 newPosition2D = Random.insideUnitCircle;
-			Vector3 newPosition3D = transform.position;
-			newPosition3D.x += newPosition2D.x;
-			newPosition3D.y = 3;
-			newPosition3D.z += newPosition2D.y;
-			Instantiate(creature, newPosition3D, transform.rotation);
+			float distance = (transform.position - target.transform.position).magnitude;
+			if (distance < minDistance) {
+				Vector2 newPosition2D = Random.insideUnitCircle;
+				Vector3 newPosition3D = transform.position;
+				newPosition3D.x += newPosition2D.x;
+				newPosition3D.y = 3;
+				newPosition3D.z += newPosition2D.y;
+				Instantiate(creature, newPosition3D, transform.rotation);
+			}
 			yield return new WaitForSeconds(interval);
 		}
 	}
