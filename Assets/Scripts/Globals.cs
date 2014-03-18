ae9a53d6fd67668cf59040cd7528f928435551b6
Christian Sasso <christian.sasso@gmail.com>
2014-03-18 01:20:33 -0700

    Added initial menu with Play Options and Exit items with the Options one, that allows choosing one of the 4 characeters not implemented yet.

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Globals : MonoBehaviour {
 	static public int numLives = 3; // FIXME: max 9 lives?
-	static public int currentLevel = 0; // into level
+	static public int currentLevel = 0; // menu level
 	static public float playerEnergy = 100f; //min is 1, max 100, 0 means death
 	static public int score = 0; 
 }
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public Texture btnTexture;
+
+	void OnGUI() {
+
+		GUI.BeginGroup(new Rect(Screen.width / 2, Screen.height / 2 - 150, 800, 600));
+
+		if (GUI.Button(new Rect(10, 10, 50, 50), "Play")) {
+			Debug.Log("Clicked the Play button");
+			Globals.currentLevel = 1;
+			Application.LoadLevel(Globals.currentLevel); // first level
+		}
+
+		if (GUI.Button(new Rect(10, 70, 50, 50), "Options")) {
+			Debug.Log("Clicked the Options button");
+
+		}
+
+		if (GUI.Button(new Rect(10, 130, 50, 50), "Exit")) {
+			Debug.Log("Clicked the Exit button");
+			Application.Quit();
+		}
+
+		GUI.EndGroup();
+	}
+}
