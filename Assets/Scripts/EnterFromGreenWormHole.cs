42b1699f321e0378edc2d739079834e5fcc289ee
Christian Sasso <christian.sasso@gmail.com>
2014-03-25 00:43:23 -0700

    Added unidirectional teleporters.

---
+++ Assets/Scripts/EnterFromGreenWormHole.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterFromGreenWormHole : MonoBehaviour {
+	
+	public string collidedTo;
+	public GameObject toGreenWormHole;
+
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log("OnCollisionEnter: " + collision.gameObject.tag + " - " + collidedTo);
+		if (collision.gameObject.tag == collidedTo)
+		{
+			collision.gameObject.transform.position = toGreenWormHole.transform.position;
+		}
+	}
+}
---
+++ Assets/Scripts/EnterFromRedWormHole.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterFromRedWormHole : MonoBehaviour {
+	
+	public string collidedTo;
+	public GameObject toRedWormHole;
+
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log("OnCollisionEnter: " + collision.gameObject.tag + " - " + collidedTo);
+		if (collision.gameObject.tag == collidedTo)
+		{
+			collision.gameObject.transform.position = toRedWormHole.transform.position;
+		}
+	}
+}
