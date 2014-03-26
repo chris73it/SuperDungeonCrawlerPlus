ff5eba048580a541d7975fde80240a6c8b7a2e06
Christian Sasso <christian.sasso@gmail.com>
2014-03-26 00:59:58 -0700

    Added nest+enemies combo of different color/shape and added the position to restore 30 points of energy.

---
+++ Assets/Scripts/PickUpPotion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpPotion : MonoBehaviour {
+
+	public string collidedTo;
+	
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log ("OnCollisionEnter: " + collision.gameObject.tag + " - " + collidedTo);
+		if (collision.gameObject.tag == collidedTo)
+		{
+			Destroy (gameObject);
+			Globals.playerEnergy += 30f;
+			if (Globals.playerEnergy > 100f) {
+				Globals.playerEnergy = 100f;
+			}
+		}
+	}
+}
