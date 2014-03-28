45d114f5fa2b6248ebca1f8ddafd444d6285d0b6
Christian Sasso <christian.sasso@gmail.com>
2014-03-28 00:17:38 -0700

    Added pink key and teleporter to implement the challenge on the very first screen.

---
+++ Assets/Scripts/PickUpPinkKey.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpPinkKey : MonoBehaviour {
+
+	static public bool gotPinkKey = false;
+
+	public string collidedTo;
+
+	GameObject centralPillar;
+
+	void Start() {
+		centralPillar = GameObject.Find("CentralPillar");
+		if (centralPillar) Debug.Log("centralPillar is NOT NULL");
+		else Debug.Log("centralPillar is NULL");
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log ("OnCollisionEnter: " + collision.gameObject.tag + " - " + collidedTo);
+		if (collision.gameObject.tag == collidedTo)
+		{
+			gotPinkKey = true;
+			if (centralPillar) Debug.Log("NOT NULL: " + centralPillar);
+			Destroy(centralPillar);
+			Destroy(gameObject);
+		}
+	}
+}
