ac490457d3d7ebb239a601070e2e17859deb3081
Christian Sasso <christian.sasso@gmail.com>
2014-03-28 17:16:21 -0700

    Garbage collect bullets that fall below the minimum height.

--- Assets/Scripts/DeleteOnStop.cs
+++ Assets/Scripts/DeleteOnStop.cs
@@ -5,7 +5,7 @@ public class DeleteOnStop : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (rigidbody.velocity == Vector3.zero) {
+		if (rigidbody.velocity == Vector3.zero || rigidbody.position.y < Globals.MINIMUM_HEIGHT) {
 			Destroy(gameObject); 
 		}
 	}
