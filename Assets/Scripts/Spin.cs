a3bdb2ffc2fd77ce04370e24d8ef05db7513db1d
Christian Sasso <christian.sasso@gmail.com>
2014-04-16 09:25:52 -0700

    Made all teleporters like green spinning diamonds (cubes on a 45 degree angle.)

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -5,5 +5,7 @@ public class Spin : MonoBehaviour {
 	
 	void Update () {
 		transform.Rotate(Vector3.up * Time.deltaTime * 100);
+		transform.Rotate(Vector3.forward * Time.deltaTime * 100);
+		transform.Rotate(Vector3.left * Time.deltaTime * 100);
 	}
 }
